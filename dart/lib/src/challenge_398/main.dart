afba03ea65d471beb4e9b099ea65e5e8abe5ef7a
Robert Tyssen <rob.tyssen@gmail.com>
2022-06-12 18:35:45 -0400

    Added solution to challenge #399

--- dart/lib/src/challenge_398/main.dart
+++ dart/lib/src/challenge_398/main.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:path/path.dart' as path;
 
@@ -7,19 +8,59 @@ final fileMtx5 = path.join(
 final fileMtx20 = path.join(
     Directory.current.path, 'assets', 'challenge_398', 'matrix_20x20.txt');
 
+typedef Matrix<T> = List<List<T>>;
+
 void main() async {
   var fileToUse = fileMtx5;
-  var matrix = getMatrixFromFile(fileToUse);
+  var mtx = await getMatrixFromFile(fileToUse);
+
+  optimalSum(mtx);
 }
 
-Future<List<List<int>>> getMatrixFromFile(String filename) async {
+Future<Matrix<int>> getMatrixFromFile(String filename) async {
   var file = File(filename);
   var lines = await file.readAsLines();
 
   var matrix = lines
-      .map((line) => line.split(' ').map((e) => int.parse(e)).toList())
+      .map((line) =>
+          line.split(RegExp('\\s+')).map((e) => int.parse(e)).toList())
       .toList();
 
   return matrix;
 }
 
+void optimalSum(Matrix<int> mtx) {
+  var rows = mtx.length;
+  var cols = mtx[0].length;
+
+  Matrix<bool> starred = List.filled(rows, List.filled(cols, false));
+
+  print(mtx);
+
+  for (var i = 0; i < rows; i++) {
+    var rowMin = mtx[i].reduce(min);
+    for (var j = 0; j < cols; j++) {
+      mtx[i][j] -= rowMin;
+    }
+  }
+
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < cols; j++) {
+      
+    }
+  }
+
+  print(mtx);
+}
+
+List<int> getColumn(List<List<int>> mtx, int i) {
+  return mtx.map((e) => e[i]).toList();
+}
+
+Matrix removeColumn(Matrix mtx, int i) {
+  return mtx.map((e) {
+    var row = List<int>.from(e);
+    row.remove(i);
+    return row;
+  }).toList();
+}
---
+++ dart/lib/src/challenge_399/challenge_399.dart
@@ -0,0 +1,47 @@
+import 'package:test/test.dart';
+
+final _letterMap = {
+  'a': 1,
+  'b': 2,
+  'c': 3,
+  'd': 4,
+  'e': 5,
+  'f': 6,
+  'g': 7,
+  'h': 8,
+  'i': 9,
+  'j': 10,
+  'k': 11,
+  'l': 12,
+  'm': 13,
+  'n': 14,
+  'o': 15,
+  'p': 16,
+  'q': 17,
+  'r': 18,
+  's': 19,
+  't': 20,
+  'u': 21,
+  'v': 22,
+  'w': 23,
+  'x': 24,
+  'y': 25,
+  'z': 26,
+};
+
+void main() {
+  print(_letterSum(''));
+  print(_letterSum('microspectrophotometries'));
+}
+
+int _letterSum(String word) {
+  word = word.replaceAll(RegExp('[^A-Za-z]'), '').toLowerCase();
+
+  int sum = 0;
+
+  for (int i = 0; i < word.length; i++) {
+    sum += _letterMap[word[i]] ?? 0;
+  }
+
+  return sum;
+}
